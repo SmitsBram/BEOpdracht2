6870ec10a9b8fc08f4e0635a8c70a51fbc12831f
Bram <336796@student.mboutrecht.nl>
2023-10-23 19:08:16 +0200

    Opdracht 2 af

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', '');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'register');
+define('URLROOT', 'http://Opdracht2');
+define('APPROOT', dirname(dirname(__FILE__)));
